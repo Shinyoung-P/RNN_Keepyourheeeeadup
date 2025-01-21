355e8f72173c1b6ae4b1e700d6c023ed6cec5fdc
Shinyoung-P <sypzang@gmail.com>
2025-01-22 04:18:48 +0900

    Update test-0142.py

--- test/test-0142.py
+++ test/test-0142.py
@@ -4,6 +4,7 @@ import time
 import os
 import joblib  # 모델 로드용 모듈
 import numpy as np  # 데이터 처리용 모듈
+import sklearn
 
 
 # 시리얼 포트 연결 (포트 이름과 baud rate는 환경에 맞게 수정)
@@ -18,7 +19,7 @@ time.sleep(2)
 print("시리얼 데이터 수신 시작 (Ctrl+C로 종료)")
 
 # 분류 모델 로드 (모델 파일 경로는 실제 파일 경로로 수정 필요)
-model_path = 'C:\Users\sypza\RNN_Keepyourheeeeadup\model\keepheeead-model-0142.pkl'  # 모델 경로
+model_path = 'C:\\Users\\sypza\\RNN_Keepyourheeeeadup\\model\\keepheeead-model-0142.pkl'
 try:
     classifier = joblib.load(model_path)
     print("모델 로드 성공")
@@ -33,7 +34,9 @@ try:
     while True:
         # 한 줄씩 읽어 들임 (바이트 형태)
         try:
+            print("AAAAAA")
             raw_line = ser.readline()
+            print("BBBBBB")
             decoded_line = raw_line.decode('utf-8', errors='replace').strip()
 
             # 구분자 '|'를 기준으로 데이터 분리
@@ -49,25 +52,27 @@ try:
         # 문자열이 있을 경우 처리
         if line:
             values = line.split(',')
-            if len(values) == 13:
+            if len(values) == 10:
                 try:
+                    print("CCCCCCC")
                     # 숫자 데이터 변환; 변환 실패 시 해당 행 무시
-                    roll1, pitch1, roll2, pitch2, roll3, pitch3, roll4, pitch4, \
+                    roll1, pitch1, roll2, pitch2, roll3, pitch3, roll4, pitch4,\
                     roll5, pitch5, roll6, pitch6, FSR = map(float, line.split(','))
                     FSR = int(FSR)
 
                     # 모델 입력 데이터 준비
                     input_features = np.array([roll1, pitch1, roll2, pitch2, roll3, pitch3, 
-                                                roll4, pitch4, roll5, pitch5, roll6, pitch6, FSR]).reshape(1, -1)
+                                                roll5, roll6, pitch6, FSR]).reshape(1, -1)
 
+                    print("DDDDDDD")
                     # 분류 모델 적용
                     predicted_class = classifier.predict(input_features)[0]
                 except ValueError:
                     continue
 
+                print("FFFFFFFF")
                 # 터미널에 출력
-                print(t, roll1, pitch1, roll2, pitch2, roll3, pitch3, roll4, 
-                      pitch4, roll5, pitch5, roll6, pitch6, FSR, predicted_class)
+                print(t, roll1, pitch1, roll2, pitch2, roll3, pitch3, roll5, roll6, pitch6, FSR, predicted_class)
 
                 t += 1
 
